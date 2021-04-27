5789c7cd89a0d5cf1b17a5eb776ef5f9ac30d88b
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 15:08:00 -0400

    added before_action require_login for user elements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  
-  private
-
-  def require_login
-    return head(:forbidden) unless session.include? :user_id
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :require_login, only: [:show]
+
   def show
     @user = User.find_by(id: session[:user_id])
+  end
+
+  private
+
+  def require_login
+    redirect_to login_path unless session.include?(:user_id)
   end
 end
