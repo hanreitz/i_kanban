4b4acb319b8b0c0c3622d3e42781a5c885fc7db3
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 14:19:53 -0400

    wrote helper method for requiring login in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method require_login
+  
+  private
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
 end
