7b128355e9452d20e578b996c72d53b3ef074ac5
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 16:03:21 -0400

    users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new, alert: "User could not be created."
+    end
   end
-  
+
   def show
     @user = helpers.current_user
   end
