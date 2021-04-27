59c907a250d17de195deec177383bbaaee937783
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 14:29:52 -0400

    login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def new
+  end
+  
   def create
     @user = User.find_by(username: params[:user][:username])
     return head(:forbidden) unless @user.authenticate(params[:password])
