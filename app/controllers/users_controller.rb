cf4a3c18c333d8cb28c00a91a08ac843b81e6785
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 16:00:41 -0400

    added strong params and basic new and create methods to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
   before_action :require_login, only: [:show]
 
+  def new
+  end
+
+  def create
+  end
+  
   def show
     @user = helpers.current_user
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :organization, :password, :public)
+  end
 
   def require_login
     redirect_to login_path, alert: "You must be logged in to view this page." unless session.include?(:user_id)
