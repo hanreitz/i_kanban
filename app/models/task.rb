705643b97e870478d96657d7463454c9bd51973c
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 11:23:45 -0400

    added validations to models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  validates_presence_of :due_date, :description, :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :tasks
   has_many :projects, through: :tasks
+
+  has_secure_password
+  validates_presence_of :username, :email, :first_name, :last_name
+  validates_uniqueness_of :username, :email
 end
