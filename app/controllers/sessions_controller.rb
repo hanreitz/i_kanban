bee26aa7cbde86210120545735ca3502503b4c38
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 14:52:54 -0400

    basic user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
   
   def create
-    @user = User.find_by(username: params[:user][:username])
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    render user_path(@user)  
+    user = User.find_by(username: params[:user][:username])
+    return head(:forbidden) unless user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect_to user_path(user)
   end
 
   # def create_from_omniauth
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   validates_presence_of :username, :email, :first_name, :last_name
   validates_uniqueness_of :username, :email
   validates_length_of :password, in: 6..20, message: "Password must be between 6 and 20 characters."
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "Email address must be valid."
+  # validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "Email address must be valid."
 end
