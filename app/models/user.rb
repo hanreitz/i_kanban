f26c3b91b3cba2f13ff012c37692a46cccf344f8
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 14:19:19 -0400

    aadded validations for password length and email in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :username, :email, :first_name, :last_name
   validates_uniqueness_of :username, :email
+  validates_length_of :password, in: 6..20, message: "Password must be between 6 and 20 characters."
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "Email address must be valid."
 end
