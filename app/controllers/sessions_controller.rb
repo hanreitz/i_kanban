4eb403d6ad7284732e0e4c44e519801314ab5fc7
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 14:24:01 -0400

    wrote sessions#create and sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
+    @user = User.find_by(username: params[:user][:username])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
   end
 
   def destroy
+    session[:user_id] = nil
   end
 end
