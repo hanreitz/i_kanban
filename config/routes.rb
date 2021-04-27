e16b9c6934e853dc204755b72a6ba1cc0aaa673d
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-27 17:43:21 -0400

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+  get '/user/:id/account', to: 'users#account'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
