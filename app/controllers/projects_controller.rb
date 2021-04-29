f0daf0dd1d167f5058e1cea8a4ef911051be2edd
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-29 17:07:48 -0400

    added error messages to new project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,7 +8,22 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+  def create
+    @project = Project.create(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new, alert: "Project could not be created."
+    end
+  end
+
   def show 
     @project = Project.find_by(id: params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :organization, :owner)
   end
 end
