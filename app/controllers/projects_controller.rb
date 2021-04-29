dd949e48ffda923bf62bd90afa45486002b8e334
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-29 16:58:07 -0400

    new project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,14 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all
+    @projects = Project.public_projects
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def show 
+    @project = Project.find_by(id: params[:id])
   end
 end
