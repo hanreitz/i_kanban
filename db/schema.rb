a13b651c99922ca2b65f6081ea87c9fd954f5f03
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-29 16:39:33 -0400

    added owner to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_152821) do
+ActiveRecord::Schema.define(version: 2021_04_29_203714) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_04_27_152821) do
     t.string "organization"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "owner"
   end
 
   create_table "tasks", force: :cascade do |t|
