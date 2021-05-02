4edf225281e080b3d9a68055beb3d1348a206930
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-02 16:25:38 -0400

    wrote nested projects/tasks routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   resources :tasks
-  resources :projects
+  resources :projects, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :users
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   get '/user/:id/account', to: 'users#account', as: 'user_account'
+
+  resources :projects, only: [:show] do
+    resources :tasks, only: [:index, :new, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
