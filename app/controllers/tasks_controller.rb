c8d9ebe20db048917f45c2a1cdf3eb7884302820
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-02 16:22:56 -0400

    basic TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,24 @@
 class TasksController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def task_params
+  end
 end
