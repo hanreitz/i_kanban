7627a861c4844d9e3c36bb2b57e978c35a178e64
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-02 17:51:26 -0400

    tasks#create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,26 @@
 class TasksController < ApplicationController
   def index
+    if params[:project_id]
+      @tasks = Project.find_by(id: params[:project_id]).tasks
+    else
+      @tasks = Task.all
+    end
   end
 
   def new
+    @task = Task.new(project_id: params[:project_id], user_id: current_user.id)
   end
 
   def create
+    @task = Task.create(task_params)
+    if @task.save
+      redirect_to project_tasks_path(@task.project)
+    else
+      render :new, alert: "Task could not be created."
+    end
+  end
+
+  def show
   end
 
   def edit
