e0e094a2f12ffd3e320cf39df7cc5a084edddbc1
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-07 11:34:53 -0400

    fixed OAuth issue by adding on: :create to password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
   validates_presence_of :username, :email, :first_name, :last_name
   validates_uniqueness_of :username, :email
-  # validates_length_of :password, minimum: 6, message: " must be at least 6 characters."
+  validates_length_of :password, in: 6..40, on: :create, message: " must be 6-40 characters."
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "Email address must be valid."
 
   scope :public_users, -> { where(public: true) }
