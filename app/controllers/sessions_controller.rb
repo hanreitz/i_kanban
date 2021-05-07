4fbd82891d1c41bfb35646fe9854bc1201328266
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-06 21:03:59 -0400

    wrote SessionsController #omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,20 @@ class SessionsController < ApplicationController
     redirect_to user_path(user)
   end
 
-  # def create_from_omniauth
-  # end
+  def omniauth
+    byebug
+    user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider: request.env['omniauth.auth'][:provider]) do |u|
+      u.username = request.env['omniauth.auth'][:info][:email]
+      u.first_name = request.env['omniauth.auth'][:info][:first_name]
+      u.last_name = request.env['omniauth.auth'][:info][:last_name]
+      u.password = SecureRandom.hex(15)
+    end
+    if user.save
+      #login and redirect
+    else
+      redirect_to login_path
+    end
+  end
 
   def destroy
     session.clear
