61d5bd7ff4c18523e0d35802f397ab3d345e9f9a
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-06 20:37:57 -0400

    added uid and provider attributes to users table for omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_150816) do
+ActiveRecord::Schema.define(version: 2021_05_07_003537) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2021_05_06_150816) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "uid"
+    t.string "provider"
   end
 
 end
