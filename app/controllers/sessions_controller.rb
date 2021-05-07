184edbb0e0b2e4daa321ac37e1401d0c21acfad9
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-06 22:52:35 -0400

    Google OAuth added; password length validation removed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,17 +16,18 @@ class SessionsController < ApplicationController
   end
 
   def omniauth
-    byebug
     user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider: request.env['omniauth.auth'][:provider]) do |u|
       u.username = request.env['omniauth.auth'][:info][:email]
       u.first_name = request.env['omniauth.auth'][:info][:first_name]
       u.last_name = request.env['omniauth.auth'][:info][:last_name]
+      u.email = request.env['omniauth.auth'][:info][:email]
       u.password = SecureRandom.hex(15)
     end
-    if user.save
-      #login and redirect
+    if user.valid?
+      session[:user_id] = user.id
+      redirect_to user_path(user)
     else
-      redirect_to login_path
+      redirect_to login_path, alert: "#{user.errors.full_messages[0]}"
     end
   end
 
